3c6e444a6288db8b36e3e7521c40c7fa0b31e066
Sboakye-uofsc <SBOAKYE@email.sc.edu>
2025-11-16 22:22:40 -0500

    created 242 file and home page

--- src/components/ReviewDetail.jsx
+++ src/components/ReviewDetail.jsx
@@ -9,32 +9,38 @@ const ReviewDetail = () => {
   const { id } = useParams();
 
   useEffect(() => {
-    // Fetch review based on the ID from the URL
-    fetch(`https://statistics-backend-jjpb.onrender.com/api/Review/${id}`)
-      .then(response => response.json())
-      .then(data => {
-        setReview(data);
-      })
-      .catch(error => {
-        console.error('Error fetching review:', error);
-      });
-  }, [id]);
+	fetch(`https://statistics-backend-jjpb.onrender.com/api/Review/${id}`)
+		.then(response => response.json())
+		.then(data => {
+		console.log('Review data:', data); // Check what fields exist
+		setReview(data);
+		})
+		.catch(error => {
+		console.error('Error fetching review:', error);
+		});
+}, [id]);
 
   if (!review) return <main><p>Loading...</p></main>;
 
+
   return (
-    <main>
-      <h1>{review.name_game}</h1>
-      <img src={review.img} alt={review.name_game} />
-      <p>Username: {review.username}</p>
-      <p>Rating: {review.rating}</p>
-      <p>{review.description}</p>
-      <p>Review: {review.reviews[0]}</p>
-	  <section>
-		<Comments/>
-	  </section>
-    </main>
+    <div>
+		<img 
+			src={`https://statistics-backend-jjpb.onrender.com${review.img}`}
+			alt={review.name_game}
+			style={{ maxWidth: '500px', width: '100%', height: 'auto' }}
+		/>
+
+		<p>Username: {review.username}</p>
+		<p>Rating: {review.rating}</p>
+		<p>{review.description}</p>
+		<p>Review: {review.reviews[0]}</p>
+		<section>
+			<Comments />
+		</section>
+    </div>
   );
+
 };
 
 export default ReviewDetail;
